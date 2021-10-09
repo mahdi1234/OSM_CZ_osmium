c1a42ac20a1c80c7de6821e06ea447fa2872a38c
mahdi1234 <mahdi1234@centrum.cz>
2021-10-09 22:22:48 +0000

    Add files via upload

---
+++ umap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#wget http://download.geofabrik.de/europe/czech-republic-latest.osm.pbf
+osmium tags-filter ./czech-republic-latest.osm.pbf nwr/recycling_type=centre -o recycling_centre.pbf
+osmium export recycling_centre.pbf -o recycling_centre.geojson
+cat recycling_centre.geojson | grep -i '"recycling_type":"centre"' | grep -vi "recycling:" > undefined_recycling_centre.geojson
+#rm ./czech-republic-latest.osm.pbf

1f9093b1142c35771f5a88c7a4eb5560f9d9c17c
mahdi1234 <mahdi1234@centrum.cz>
2021-10-09 23:31:51 +0000

    Update umap.sh

--- umap.sh
+++ umap.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# get latest country file
 wget http://download.geofabrik.de/europe/czech-republic-latest.osm.pbf
+
+# export ecycling_type=centre without recycling: details
 osmium tags-filter ./czech-republic-latest.osm.pbf nwr/recycling_type=centre -o recycling_centre.pbf
-osmium export recycling_centre.pbf -o recycling_centre.geojson
-echo '{"type":"FeatureCollection","features":[' > undefined_recycling_centre.geojson
-cat recycling_centre.geojson | grep -i '"recycling_type":"centre"' | grep -vi "recycling:" >> undefined_recycling_centre.geojson
-echo ']}' >> undefined_recycling_centre.geojson
+osmium export recycling_centre.pbf --overwrite -o recycling_centre.json
+cat recycling_centre.geojson | grep -i '"recycling_type":"centre"' | grep -vi "recycling:" > undefined_recycling_centre.geojson
+
+## annotate all geojson files
+sed -i '1 i\\{"type":"FeatureCollection","features":[' *.geojson
+sed -i '$a ]}' *.geojson
 rm ./czech-republic-latest.osm.pbf

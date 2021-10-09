3b42ed78e238d1eb4fe7cc0de2ed8f6a7bd43e30
mahdi1234 <mahdi1234@centrum.cz>
2021-10-09 22:45:09 +0000

    Update umap.sh

--- umap.sh
+++ umap.sh
@@ -2,5 +2,7 @@
 wget http://download.geofabrik.de/europe/czech-republic-latest.osm.pbf
 osmium tags-filter ./czech-republic-latest.osm.pbf nwr/recycling_type=centre -o recycling_centre.pbf
 osmium export recycling_centre.pbf -o recycling_centre.geojson
-cat recycling_centre.geojson | grep -i '"recycling_type":"centre"' | grep -vi "recycling:" > undefined_recycling_centre.geojson
+echo '{"type":"FeatureCollection","features":[' > undefined_recycling_centre.geojson
+cat recycling_centre.geojson | grep -i '"recycling_type":"centre"' | grep -vi "recycling:" >> undefined_recycling_centre.geojson
+echo ']}' >> undefined_recycling_centre.geojson
 rm ./czech-republic-latest.osm.pbf

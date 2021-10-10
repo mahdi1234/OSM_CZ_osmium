7049101eeeccb8c867509a593b9119cd59ad5473
mahdi1234 <mahdi1234@centrum.cz>
2021-10-10 18:36:05 +0000

    Add files via upload

--- download.sh
+++ download.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget http://download.geofabrik.de/europe/czech-republic-latest.osm.pbf
+#wget -O extract.pbf http://download.geofabrik.de/europe/czech-republic-latest.osm.pbf
+osmium extract -p cezch_republic.geojson extract.pbf --overwrite -o czech-republic-latest.osm.pbf
--- remove.sh
+++ remove.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 rm ./czech-republic-latest.osm.pbf
+rm ./extract.pbf

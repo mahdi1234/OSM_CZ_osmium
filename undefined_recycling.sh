d8bd75f71b141672f404de0fa406e0995a1dd488
mahdi1234 <mahdi1234@centrum.cz>
2021-10-10 20:46:33 +0000

    Add files via upload

--- undefined_recycling.sh
+++ undefined_recycling.sh
@@ -12,6 +12,11 @@ osmium tags-filter ./czech-republic-latest.osm.pbf nwr/recycling_type=container 
 osmium export undefined_recycling/recycling_container.pbf --overwrite -o undefined_recycling/recycling_container.json
 cat undefined_recycling/recycling_container.json | grep -e '"recycling_type":"container"' -e 'FeatureCollection' -e '^]}$' | grep -vi "recycling:" | tac | sed '2s/,$//' | tac | jq . > ./undefined_recycling/undefined_recycling_container.geojson
 
+echo "Recycling no type"
+osmium tags-filter ./czech-republic-latest.osm.pbf nwr/amenity=recycling --overwrite -o undefined_recycling/recycling_no_type.pbf
+osmium export undefined_recycling/recycling_no_type.pbf --overwrite -o undefined_recycling/recycling_no_type.json
+cat undefined_recycling/recycling_no_type.json | grep -e '"amenity":"recycling"' -e 'FeatureCollection' -e '^]}$' | grep -vi "recycling_type" | tac | sed '2s/,$//' | tac | jq . > ./undefined_recycling/recycling_no_type.geojson
+
 #echo "Process all jsons"
 #sed -i '1 i\\{"type":"FeatureCollection","features":[' ./undefined_recycling/*.geojson
 #sed -i '$a ]}' undefined_recycling/*.geojson
@@ -23,9 +28,11 @@ cat ./undefined_recycling/undefined_recycling_centre.geojson | grep 'centre' | w
 echo "Kontejnery" >> ./undefined_recycling/progress.txt
 cat ./undefined_recycling/undefined_recycling_container.geojson | grep 'container' | wc -l >> ./undefined_recycling/progress.txt
 echo "Bez typu" >> ./undefined_recycling/progress.txt
+cat ./undefined_recycling/recycling_no_type.geojson | grep 'amenity' | wc -l >> ./undefined_recycling/progress.txt
 
 echo "Create gpx"
 mkdir -p undefined_recycling/gpx
 gpsbabel -i geojson -f ./undefined_recycling/undefined_recycling_centre.geojson -o gpx -F ./undefined_recycling/gpx/undefined_recycling_centre.gpx
 gpsbabel -i geojson -f ./undefined_recycling/undefined_recycling_container.geojson -o gpx -F ./undefined_recycling/gpx/undefined_recycling_container.gpx
+gpsbabel -i geojson -f ./undefined_recycling/recycling_no_type.geojson -o gpx -F ./undefined_recycling/gpx/recycling_no_type.gpx
 sed -i '/.*time.*/d' undefined_recycling/gpx/*.gpx

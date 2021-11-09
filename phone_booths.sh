95ff69177ebcc7cd308eafa900270a74b37ff1a2
mahdi1234 <mahdi1234@centrum.cz>
2021-11-09 22:48:17 +0000

    Add files via upload

--- phone_booths.sh
+++ phone_booths.sh
@@ -3,15 +3,16 @@
 mkdir -p phone_booths
 
 echo "Active phone booths"
-osmium tags-filter ./czech-republic-latest.osm.pbf nwr/*=telephone --overwrite -o phone_booths/all_phone_booths.pbf
-osmium export phone_booths/all_phone_booths.pbf --overwrite -o phone_booths/all_phone_booths.json -c osmium_options.json
-cat phone_booths/all_phone_booths.json | grep -e '"amenity":"telephone"' -e 'FeatureCollection' -e '^]}$' | tac | sed '2s/,$//' | tac | jq . > ./phone_booths/active_phone_booths.geojson
+osmium tags-filter ./czech-republic-latest.osm.pbf nwr/*=telephone --overwrite -o ./phone_booths/all_phone_booths.pbf
+osmium export ./phone_booths/all_phone_booths.pbf --overwrite -o ./phone_booths/all_phone_booths_temp.json -c osmium_options.json
+cat ./phone_booths/all_phone_booths_temp.json | grep -vi 'LineString' > /phone_booths/all_phone_booths.json
+cat ./phone_booths/all_phone_booths.json | grep -e '"amenity":"telephone"' -e 'FeatureCollection' -e '^]}$' | tac | sed '2s/,$//' | tac | jq . > ./phone_booths/active_phone_booths.geojson
 
 echo "Disused phone booths"
-cat phone_booths/all_phone_booths.json | grep -e '"disused:amenity":"telephone"' -e '"historic:amenity":"telephone"' -e 'FeatureCollection' -e '^]}$' | grep -vi "check_date" | grep -vi "survey_date" | tac | sed '2s/,$//' | tac | jq . > ./phone_booths/disused_phone_booths.geojson
+cat ./phone_booths/all_phone_booths.json | grep -e '"disused:amenity":"telephone"' -e '"historic:amenity":"telephone"' -e 'FeatureCollection' -e '^]}$' | grep -vi "check_date" | grep -vi "survey_date" | tac | sed '2s/,$//' | tac | jq . > ./phone_booths/disused_phone_booths.geojson
 
 echo "Verified phone booths"
-cat phone_booths/all_phone_booths.json | grep -e '"survey:date"' -e '"check_date"' -e 'FeatureCollection' -e '^]}$' | tac | sed '2s/,$//' | tac | jq . > ./phone_booths/verified_phone_booths.geojson
+cat ./phone_booths/all_phone_booths.json | grep -e '"survey:date"' -e '"check_date"' -e 'FeatureCollection' -e '^]}$' | tac | sed '2s/,$//' | tac | jq . > ./phone_booths/verified_phone_booths.geojson
 
 echo "Dump progress stats"
 > ./phone_booths/progress.txt

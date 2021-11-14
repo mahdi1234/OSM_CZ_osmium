c87f35cf70420c30462593837d6888763b85e74e
mahdi1234 <mahdi1234@centrum.cz>
2021-11-14 21:24:40 +0000

    Add files via upload

--- osm_varios.sh
+++ osm_varios.sh
@@ -70,4 +70,4 @@ echo '{"type":"FeatureCollection","features":[' > ./osm_varios/uzavirky_final.js
 cat ./osm_varios/uzavirky_aktualni.json >> ./osm_varios/uzavirky_final.json
 echo ']}' >> ./osm_varios/uzavirky_final.json
 cat ./osm_varios/uzavirky.json | perl -pe 's/(.*no \@.*?\-.*?)(\d\d\d\d.*?\d\d)(.*)/$1$2$3\t$2/' | dateutils.dconv -i "%Y %b %d" -f %Y%m%d -S | awk 'BEGIN { FS="\t" } $2<='$today' {print $1}' | grep -v '"vehicle:conditional":"delivery @' > ./osm_varios/uzavirky_neaktualni.json
-cat ./osm_varios/uzavirky_neaktualni.json | perl -pe 's/.*?\@type\"\:\"(.*?)\".*?\@id\"\:(.*?)\,.*/$1$2/' | perl -pe 's/way/w/'| perl -pe 's/node/n/' | perl -pe 's/(.*)/curl \"http\:\/\/localhost\:8111\/load_object\?new_layer=false\&objects=$1\"/' > ./osm_varios/uzavirky_JOSM.txt
+cat ./osm_varios/uzavirky_neaktualni.json | ./josmize.sh > ./osm_varios/uzavirky_JOSM.txt

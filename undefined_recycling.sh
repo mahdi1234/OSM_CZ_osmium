c70e240dd29d8894c9ceaf53dfe59ff814bfbaea
mahdi1234 <mahdi1234@centrum.cz>
2022-01-11 11:21:52 +0000

    Add files via upload

--- undefined_recycling.sh
+++ undefined_recycling.sh
@@ -22,6 +22,12 @@ cat ./undefined_recycling/recycling_no_type.json | grep -e '"amenity":"recycling
 echo '#!/bin/bash' > ./undefined_recycling/recycling_no_type_JOSM.sh
 cat ./undefined_recycling/recycling_no_type.json | grep -e '"amenity":"recycling"' | grep -vi "recycling_type" | ./josmize.sh >> ./undefined_recycling/recycling_no_type_JOSM.sh
 
+echo "Recycling: with no amenity or recycling_type"
+osmium tags-filter ./czech-republic-latest.osm.pbf nwr/recycling:* --overwrite -o undefined_recycling/recycling_type_no_amenity.pbf
+osmium export undefined_recycling/recycling_type_no_amenity.pbf --overwrite -o undefined_recycling/recycling_type_no_amenity_temp.json -c osmium_options.json
+cat ./undefined_recycling/recycling_type_no_amenity_temp.json | ./urlize.sh | grep -vi 'LineString' > ./undefined_recycling/recycling_type_no_amenity.json
+cat ./undefined_recycling/recycling_type_no_amenity.json | grep -e '"recycling:' -e 'FeatureCollection' -e '^]}$' | grep -vi "recycling_type"  | grep -vi '"amenity":"recycling"' | tac | sed '2s/,$//' | tac | jq . > ./undefined_recycling/recycling_type_no_amenity.geojson
+
 echo "Dump progress stats"
 > ./undefined_recycling/progress.txt
 echo "Sberna strediska" >> ./undefined_recycling/progress.txt
@@ -30,10 +36,13 @@ echo "Kontejnery" >> ./undefined_recycling/progress.txt
 cat ./undefined_recycling/undefined_recycling_container.geojson | grep 'container' | wc -l >> ./undefined_recycling/progress.txt
 echo "Bez typu" >> ./undefined_recycling/progress.txt
 cat ./undefined_recycling/recycling_no_type.geojson | grep 'amenity' | wc -l >> ./undefined_recycling/progress.txt
+echo "recycling:* bez amenity + bez recycling_type" >> ./undefined_recycling/progress.txt
+cat ./undefined_recycling/recycling_type_no_amenity.geojson | grep '"Feature"' | wc -l >> ./undefined_recycling/progress.txt
 
 echo "Create gpx"
 mkdir -p undefined_recycling/gpx
 gpsbabel -i geojson -f ./undefined_recycling/undefined_recycling_centre.geojson -o gpx -F ./undefined_recycling/gpx/undefined_recycling_centre.gpx
 gpsbabel -i geojson -f ./undefined_recycling/undefined_recycling_container.geojson -o gpx -F ./undefined_recycling/gpx/undefined_recycling_container.gpx
 gpsbabel -i geojson -f ./undefined_recycling/recycling_no_type.geojson -o gpx -F ./undefined_recycling/gpx/recycling_no_type.gpx
+gpsbabel -i geojson -f ./undefined_recycling/recycling_type_no_amenity.geojson -o gpx -F ./undefined_recycling/gpx/recycling_known_without_amanity_type.gpx
 sed -i '/.*time.*/d' ./undefined_recycling/gpx/*.gpx

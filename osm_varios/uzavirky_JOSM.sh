fdd551447601537cf2119e96dffa52f863a6f790
Automated <actions@users.noreply.github.com>
2022-04-17 09:02:52 +0000

    Latest data: Sun Apr 17 09:02:52 UTC 2022

--- osm_varios/uzavirky_JOSM.sh
+++ osm_varios/uzavirky_JOSM.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+curl "http://localhost:8111/load_object?new_layer=false&objects=w8072072"
 curl "http://localhost:8111/load_object?new_layer=false&objects=w50228785"
 curl "http://localhost:8111/load_object?new_layer=false&objects=w50228787"
 curl "http://localhost:8111/load_object?new_layer=false&objects=w55007818"
+curl "http://localhost:8111/load_object?new_layer=false&objects=w133721350"
 curl "http://localhost:8111/load_object?new_layer=false&objects=w148449284"
 curl "http://localhost:8111/load_object?new_layer=false&objects=w216255553"
 curl "http://localhost:8111/load_object?new_layer=false&objects=w392672422"

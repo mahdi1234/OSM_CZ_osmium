ffd6ea8397be20d4887c0af9c066e7a3870d2e5b
Automated <actions@users.noreply.github.com>
2023-02-13 05:03:43 +0000

    Latest data: Mon Feb 13 05:03:43 UTC 2023

--- osm_varios/brno_container_JOSM.sh
+++ osm_varios/brno_container_JOSM.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 curl "http://localhost:8111/load_object?new_layer=false&objects=n2916428484"
+curl "http://localhost:8111/load_object?new_layer=false&objects=n9870206185"
+curl "http://localhost:8111/load_object?new_layer=false&objects=n10651406005"
--- undefined_recycling/recycling_no_type_JOSM.sh
+++ undefined_recycling/recycling_no_type_JOSM.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 curl "http://localhost:8111/load_object?new_layer=false&objects=n10588146198"
-curl "http://localhost:8111/load_object?new_layer=false&objects=n10649521265"
-curl "http://localhost:8111/load_object?new_layer=false&objects=n10649521266"
-curl "http://localhost:8111/load_object?new_layer=false&objects=n10649521267"
-curl "http://localhost:8111/load_object?new_layer=false&objects=n10649521269"
-curl "http://localhost:8111/load_object?new_layer=false&objects=n10649521271"

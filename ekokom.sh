32e269d235df015c112804eff837dbb252db466b
mahdi <a@xx.zz>
2023-12-26 17:54:39 +0100

    Fixed incorrect total for Ekokom data

--- ekokom.sh
+++ ekokom.sh
@@ -6,7 +6,7 @@ echo "Amenity recycling"
 osmium tags-filter ./czech-republic-latest.osm.pbf nwr/amenity=recycling --overwrite -o ./ekokom/all_recycling.pbf
 osmium export ./ekokom/all_recycling.pbf --overwrite -o ./ekokom/all_recycling_temp.json -c ./osmium_options.json
 cat ./ekokom/all_recycling_temp.json | grep -vi 'LineString' > ./ekokom/all_recycling.json
-cat ./ekokom/all_recycling.json | grep -e '"amenity":"recycling"' -e 'FeatureCollection' -e '^]}$' | grep -ie 'recycling:' -e '"recycling_type":"centre"' -e 'FeatureCollection' -e '^]}$' | ./sanitize_json.sh > ./ekokom/filtered.json
+cat ./ekokom/all_recycling.json | grep -e '"amenity":"recycling"' -e 'FeatureCollection' -e '^]}$' | grep -ie 'recycling:' -e '"recycling_type":"centre"' -e 'FeatureCollection' -e '^]}$' | grep -e 'FeatureCollection' -e 'recycling:' -e 'FeatureCollection' -e '^]}$' | ./sanitize_json.sh > ./ekokom/filtered.json
 cat ./ekokom/filtered.json | jq . > ./ekokom/filtered.geojson
 
 echo "Dump progress stats"
@@ -18,4 +18,4 @@ cat ./ekokom/all_recycling.json | grep '"recycling_type":"container"' | wc -l >>
 echo "Sberny dvory" >> ./ekokom/progress.txt
 cat ./ekokom/all_recycling.json | grep '"recycling_type":"centre"' | wc -l >> ./ekokom/progress.txt
 echo "S typem odpadu" >> ./ekokom/progress.txt
-cat ./ekokom/filtered.geojson | grep 'amenity' | wc -l >> ./ekokom/progress.txt
+cat ./ekokom/filtered.json | grep 'amenity' | wc -l >> ./ekokom/progress.txt

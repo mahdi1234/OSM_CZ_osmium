b3a092bfcdd76e71350ea2d3bc6d20b4bf6e03c6
Automated <actions@users.noreply.github.com>
2024-05-27 15:06:04 +0000

    Latest data: Mon May 27 15:06:04 UTC 2024

---
+++ osm_varios/czech_container_no_clothes_JOSM.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl "http://localhost:8111/load_object?new_layer=false&objects=n3437787249"
+curl "http://localhost:8111/load_object?new_layer=false&objects=n5120623615"
+curl "http://localhost:8111/load_object?new_layer=false&objects=n6501160037"
+curl "http://localhost:8111/load_object?new_layer=false&objects=n7238283526"
+curl "http://localhost:8111/load_object?new_layer=false&objects=n7552363896"
+curl "http://localhost:8111/load_object?new_layer=false&objects=n9588020290"
+curl "http://localhost:8111/load_object?new_layer=false&objects=n11357125313"
+curl "http://localhost:8111/load_object?new_layer=false&objects=w1019096896"
+curl "http://localhost:8111/load_object?new_layer=false&objects=w1019096896"

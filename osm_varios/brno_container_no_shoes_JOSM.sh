1285b6793a83d052d1160d12584880e20d391e67
Automated <actions@users.noreply.github.com>
2024-11-14 04:53:24 +0000

    Latest data: Thu Nov 14 04:53:24 UTC 2024

--- osm_varios/brno_container_no_shoes_JOSM.sh
+++ osm_varios/brno_container_no_shoes_JOSM.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl "http://localhost:8111/load_object?new_layer=false&objects=n3236559612"
 curl "http://localhost:8111/load_object?new_layer=false&objects=n11951528486"
 curl "http://localhost:8111/load_object?new_layer=false&objects=n11952306202"
 curl "http://localhost:8111/load_object?new_layer=false&objects=w431558309"

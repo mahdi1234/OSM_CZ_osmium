0bcfb6ecbc9d5d78b7b596aa92faa53f5ad556fe
Automated <actions@users.noreply.github.com>
2025-05-31 04:56:24 +0000

    Latest data: Sat May 31 04:56:24 UTC 2025

--- osm_varios/brno_container_no_shoes_JOSM.sh
+++ osm_varios/brno_container_no_shoes_JOSM.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl "http://localhost:8111/load_object?new_layer=false&objects=n3236559612"
+curl "http://localhost:8111/load_object?new_layer=false&objects=n5618279099"
 curl "http://localhost:8111/load_object?new_layer=false&objects=n11951528486"
 curl "http://localhost:8111/load_object?new_layer=false&objects=n11952306202"
 curl "http://localhost:8111/load_object?new_layer=false&objects=n12396569541"

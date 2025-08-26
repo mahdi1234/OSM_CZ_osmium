2382a0b769ba69f3e8054ecb946b979fd29637b0
Automated <actions@users.noreply.github.com>
2025-08-26 04:56:08 +0000

    Latest data: Tue Aug 26 04:56:08 UTC 2025

--- undefined_recycling/recycling_no_type_JOSM.sh
+++ undefined_recycling/recycling_no_type_JOSM.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 curl "http://localhost:8111/load_object?new_layer=false&objects=n12431250579"
 curl "http://localhost:8111/load_object?new_layer=false&objects=n13022581569"
-curl "http://localhost:8111/load_object?new_layer=false&objects=n13039583263"
